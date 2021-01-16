f982ffccd3ada3ed0585c23541d4f39f85e780fd
Mehdi <gmcjeyzer@gmail.com>
2021-01-16 00:11:19 -0500

    add base side scroller

---
+++ FishPossess/Source/FishPossess/FishPossessGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FishPossessGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFishPossessGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFishPossessGameMode();
+};
+
+
+
